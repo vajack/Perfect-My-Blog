56099d69413dea0c0a3209caac75a6625833c9f3
Yuki-Ikegami <fp25657.hawks@icloud.com>
2016-12-20 14:00:19 +0900

    add templete

---
+++ Sources/Handler.swift
@@ -0,0 +1,23 @@
+import PerfectLib
+import PerfectHTTP
+import PerfectMustache
+import SQLite
+
+
+struct IndexHandler: MustachePageHandler {
+    
+    func extendValuesForResponse(context contxt: MustacheWebEvaluationContext, collector: MustacheEvaluationOutputCollector) {
+        var values = MustacheEvaluationContext.MapType()
+        values["value"] = "hello"
+        
+        contxt.extendValues(with: values)
+        do {
+            try contxt.requestCompleted(withCollector: collector)
+        } catch {
+            let response = contxt.webResponse
+            response.status = .internalServerError
+            response.appendBody(string: "\(error)")
+            response.completed()
+        }
+    }
+}
---
+++ Sources/main.swift
@@ -0,0 +1,28 @@
+import PerfectLib
+import PerfectHTTP
+import PerfectHTTPServer
+import PerfectMustache
+
+
+let server = HTTPServer()
+
+server.documentRoot = "./webroot"
+
+
+var routs = Routes()
+
+routs.add(method: .get, uri: "/", handler: {
+    request, response in
+    
+    mustacheRequest(request: request, response: response, handler: IndexHandler(), templatePath: request.documentRoot + "/index.mustache")
+})
+
+server.addRoutes(routs)
+
+server.serverPort = 8181
+
+do {
+    try server.start()
+}catch PerfectError.networkError(let err, let msg){
+    print("Network error thrown: \(err) \(msg)")
+}
